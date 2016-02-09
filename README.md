f13eb5a5764362a753b703230c1e228eba92c106
shaikathaque <shaikathaque4@gmail.com>
2016-02-09 01:40:42 -0500

    Add comments, improve code readability, add suggested future improvements

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ url: http://217.199.187.69/shaikathaque.com/ReactionTester/
 A clicking game made using JavaScript. 
 
 Shapes appear at random positions of the screen at random times. The time the user takes to click the shape after it's creation is displayed on top.
+
+Possible improvements:
+ 
+-make a feature to display the top reaction times.
+-show the results of the last 5 attempts.
+-set a difficulty level
--- scripts.js
+++ scripts.js
@@ -1,49 +1,73 @@
+	
+	//function getRandomColor() taken from stackoverflow.com
 	function getRandomColor() {
-				var letters = '0123456789ABCDEF'.split('');
-				var color = '#';
-				for (var i = 0; i < 6; i++ ) {
-					color += letters[Math.floor(Math.random() * 16)];
+		
+		var letters = '0123456789ABCDEF'.split('');
+		var color = '#';
+		
+		for (var i = 0; i < 6; i++ ) {
+			
+			color += letters[Math.floor(Math.random() * 16)];
+					
+		}
+			
+		return color;
+				
+	}
+		
+		
+	//Declare variables to calculate and output reaction time	
+	var clickedTime; var createdTime; var reactionTime;
+		
+	//Function display the box. This setTimeout function inside determines when the box is drawn, it's shape, and positioning on the screen
+		
+	function makeBox() {
+			
+		var time = Math.random();
+				
+		time*=5000; //minimum time before box is drawn is 0*5000 ms. Maximum time is .99*5000ms  which is approx 4999ms or below 5s
+				
+		setTimeout(function() {
+				
+			//decide whether shape is a box or circle
+			//math.random().0.5 means that there is a 50-50 % chance of circle or box
+			
+			if (Math.random()>0.5) {
+				//draw a circle
+				document.getElementById("box").style.borderRadius="100px"; 
+			}	else {
+					//draw a box
+					document.getElementById("box").style.borderRadius="0px"; 
 				}
 			
-				return color;
-			}
+			//position of shape from top of screen
+			var top = Math.random(); 
+			top*=300;
+					
+			//position of shape from left of screen
+			var left = Math.random(); 
+			left*= 500;
+					
+			//set positions from the top and left of the screen
+			document.getElementById("box").style.top=top+"px";
+			document.getElementById("box").style.left=left+"px";
+					
+					
+			//set colour of the shape
+			document.getElementById("box").style.backgroundColor=getRandomColor();
+			
+			//make the shape visible. The display style is 'none' by default
+			document.getElementById("box").style.display="block";
+			
+			createdTime=Date.now();
+			
+		}, time)
+			
+	} 
 		
-			var clickedTime; var createdTime; var reactionTime;
-		
-			Date.now()
-		
-			function makeBox() {
-			
-				var time = Math.random();
-				
-				time*=5000;
-				
-				setTimeout(function() {
-				
-				if (Math.random()>0.5) {
-						document.getElementById("box").style.borderRadius="100px";
-					} else {
-						document.getElementById("box").style.borderRadius="0px";
-					}
-					
-					var top = Math.random();
-					top*=300;
-					
-					var left = Math.random();
-					left*= 500;
-					
-					document.getElementById("box").style.top=top+"px";
-					document.getElementById("box").style.left=left+"px";
-					
-					document.getElementById("box").style.backgroundColor=getRandomColor();
-					document.getElementById("box").style.display="block";
-					createdTime=Date.now();
-			
-				}, time)
-			
-			}
-			
-			document.getElementById("box").onclick=function() {
+	//Recursive function
+	//When shape is clicked:
+	document.getElementById("box").onclick=function() {
 			
 				clickedTime=Date.now();
 				
@@ -55,5 +79,6 @@
 				
 				makeBox();
 			}
-			
-			makeBox();
+	
+	//Display the box
+	makeBox();
